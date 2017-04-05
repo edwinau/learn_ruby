4c450b8c80a9cbb73a6facc528bfd9adc0f0e4c0
EDWIN AU <edwinau@rogers.com>
2017-04-04 22:33:03 -0400

    ex06 in place. retweaked ex04 piglatin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,9 +1,19 @@
-def translate(word)
-  if word.start_with?('a','e','i','o','u')
-    word<<'ay'
-  elsif
-    y = word.chars.first
-    word.slice!(0)
-    return "#{word}#{y}ay"
+def translate(sentence)
+  pig_words = []
+
+  words = sentence.split(' ')
+
+  words.each do |word|
+    if word.start_with?('a','e','i','o','u')
+      word<<'ay'
+    elsif #if it only starts with one consonant subh as banada
+      y = word.chars.find
+      word.slice!(0)
+      word = "#{word}#{y}ay"
+    end
+    pig_words << word
   end
+
+  pig_words.join(' ')
+
 end
